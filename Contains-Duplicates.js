499b4ad53f64baf849406507fb937666fa1e0449
aaronbush3 <aaronbush3@gmail.com>
2022-12-12 18:17:22 -0500

    first commit

---
+++ Contains-Duplicates.js
@@ -0,0 +1,12 @@
+function containsDuplicate(arr) {
+    let numsMap = {}
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i]
+        if (numsMap[num]) {
+            return true
+        } else {
+            numsMap[num] = true
+        }
+    }
+    return false
+};
