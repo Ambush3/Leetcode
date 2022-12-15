d971fb91d14818963f8a4486002b7e78c8d3424d
aaronbush3 <aaronbush3@gmail.com>
2022-12-15 17:21:30 -0500

    Fixed mistakes

--- IntersectionOfTwoArrays.js
+++ IntersectionOfTwoArrays.js
@@ -1,10 +1,15 @@
-// find best data structure for this problem
-function intersect(nums1, nums2) {
-    const newArr = [];
-    for (let i of nums1) {
-        if (nums2.indexOf(i) > -1) { 
-            newArr.push(i) // push to new array
+// Given two integer arrays nums1 and nums2, return an array of their intersection.Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+function intersection(nums1, nums2) {
+    let result = [];
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                result.push(nums1[i]);
+                nums2.splice(j, 1);
+                break;
+            }
         }
     }
-    return newArr
+    return result;
 }
