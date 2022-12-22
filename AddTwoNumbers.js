d77b68d691ccdf4fabb1a316c2505832e7411f67
aaronbush3 <aaronbush3@gmail.com>
2022-12-22 11:40:40 -0500

    Given two linked lists in reversed order, return the sum and reverse it

---
+++ AddTwoNumbers.js
@@ -0,0 +1,16 @@
+
+function addTwoNumbers(l1, l2){
+    let carry = 0;
+    let sum = 0;
+    let head = new ListNode(0);
+    let current = head;
+    while(l1 || l2 || carry){
+        sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+    return head.next;
+}
