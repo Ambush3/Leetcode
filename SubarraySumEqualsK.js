64ed5c978d1b058310910ac83b63c88d789c5da0
aaronbush3 <aaronbush3@gmail.com>
2023-01-08 22:09:28 -0500

    Find how many subarrays equal to the sum (k)

---
+++ SubarraySumEqualsK.js
@@ -0,0 +1,14 @@
+// return the number of subarrays that equal to the sum k 
+function subArraySum(nums, k){
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        let sum = 0;
+        for(let j = i; j < nums.length; j++){
+            sum += nums[j];
+            if(sum === k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
