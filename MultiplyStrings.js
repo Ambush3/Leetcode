a214ad9486c1883cbde6c61d4bc9506ee3c31e7d
aaronbush3 <aaronbush3@gmail.com>
2023-01-15 10:09:30 -0500

    Updated algorithm

--- MultiplyStrings.js
+++ MultiplyStrings.js
@@ -1,4 +1,5 @@
+// we are using the algorithm of 0(n^2) to solve this problem
+
 var multiply = function (num1, num2) {
-    const product = +num1 * +num2
-    return product.toString()
+    return (BigInt(num1) * BigInt(num2)).toString();
 };
