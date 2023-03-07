807bfffb2b477368f5721482dea0642a82a3c848
aaronbush3 <aaronbush3@gmail.com>
2023-03-06 21:08:29 -0500

    Given a letter, or letter combos to find the excel sheet column number

---
+++ ExcelSheetColumnNumber.js
@@ -0,0 +1,10 @@
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+
+var titleToNumber = function (columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        result = result * 26 + (columnTitle.charCodeAt(i) - 64);
+    }
+    return result;
+};
