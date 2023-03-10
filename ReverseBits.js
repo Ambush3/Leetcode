31e1c8c1e7b715458cff9a97c1c7a0046ad4aabd
aaronbush3 <aaronbush3@gmail.com>
2023-03-09 20:53:17 -0500

    Reverse bits of a given 32 bits unsigned intger.

---
+++ ReverseBits.js
@@ -0,0 +1,11 @@
+// Reverse bits of a given 32 bits unsigned integer.
+
+var reverseBits = function(x) {
+    x = ((x >> 1) & 0x55555555) | ((x & 0x55555555) << 1);
+    x = ((x >> 2) & 0x33333333) | ((x & 0x33333333) << 2);
+    x = ((x >> 4) & 0x0F0F0F0F) | ((x & 0x0F0F0F0F) << 4);
+    x = ((x >> 8) & 0x00FF00FF) | ((x & 0x00FF00FF) << 8);
+    x = (x >>> 16) | (x << 16);
+
+    return x >>> 0;
+}
