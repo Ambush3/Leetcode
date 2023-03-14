1d119b5966a9b340c63e64f5c245a4ae79be8f48
aaronbush3 <aaronbush3@gmail.com>
2023-03-14 09:48:50 -0400

    Updated time complexity

--- NumberOf1Bits.js
+++ NumberOf1Bits.js
@@ -1,3 +1,15 @@
 var hammingWeight = function(n) {
     return (n.toString(2).split('1').length-1)
 };
+
+// better time complexity
+var hammingWeight = function(n) {
+    let count = 0;
+    while (n !== 0) {
+        if (n & 1 === 1) {
+            count++;
+        }
+        n >>>= 1; // Shift n right by 1 bit
+    }
+    return count;
+};
