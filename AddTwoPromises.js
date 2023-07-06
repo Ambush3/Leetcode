fcbfb2365124b331800710416049954ba686d378
aaronbush3 <aaronbush3@gmail.com>
2023-07-06 13:59:09 -0400

    Given two promises. Promise1, and Promise2. Return the sum of both

---
+++ AddTwoPromises.js
@@ -0,0 +1,6 @@
+var addTwoPromises = async function(promise1, promise2) {
+    return Promise.all([promise1, promise2]) 
+    .then(values => {
+        return values.reduce((a, b) => a + b, 0)
+    })
+};
