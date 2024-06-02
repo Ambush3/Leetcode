a92123dfe9efe9da6806a82f050d6f7c357fe0e5
aaronbush3 <aaronbush3@gmail.com>
2024-06-01 22:10:39 -0400

    Update to be more efficient

--- ScoreOfAString.js
+++ ScoreOfAString.js
@@ -3,21 +3,12 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let nums = [];
-    for (let i = 0; i < s.length; i++){
-        let asciiValue = s.charCodeAt(i);
-        nums.push(asciiValue);
+    let sumDiff = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        let asciiValue1 = s.charCodeAt(i);
+        let asciiValue2 = s.charCodeAt(i + 1);
+        let absoluteDiff = Math.abs(asciiValue1 - asciiValue2);
+        sumDiff += absoluteDiff;
     }
-
-    let sum = []
-    for (let i = 0; i < nums.length - 1; i++){
-        let absoluteDiff = Math.abs(nums[i] - nums[i + 1])
-        sum.push(absoluteDiff)
-    }
-
-    let sumDiff = 0
-    for (let i = 0; i < sum.length; i++) {
-        sumDiff += sum[i]
-    }
-    return sumDiff
+    return sumDiff;
 };
