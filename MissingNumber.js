bfed9927f7807e156967d2de822d5318c1d108f2
aaronbush3 <aaronbush3@gmail.com>
2024-06-01 22:14:44 -0400

    Update to replace var

--- MissingNumber.js
+++ MissingNumber.js
@@ -1,4 +1,4 @@
-var missingNumber = function(nums) {
+function missingNumber(nums) {
     const n = nums.length;
     const expectedSum = n * (n + 1) / 2;
     const actualSum = nums.reduce((sum, num) => sum + num, 0);
