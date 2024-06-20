99c715ed3302e3d6252930b8662affcbcc489629
aaronbush3 <aaronbush3@gmail.com>
2024-06-19 21:38:43 -0400

    Refactor to a better way to find the interesection, using Set

--- IntersectionOfTwoArrays.js
+++ IntersectionOfTwoArrays.js
@@ -1,15 +1,11 @@
-// Given two integer arrays nums1 and nums2, return an array of their intersection.Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const numsOne = new Set(nums1);
+    const numsTwo = new Set(nums2);
 
-function intersection(nums1, nums2) {
-    let result = [];
-    for (let i = 0; i < nums1.length; i++) {
-        for (let j = 0; j < nums2.length; j++) {
-            if (nums1[i] === nums2[j]) {
-                result.push(nums1[i]);
-                nums2.splice(j, 1);
-                break;
-            }
-        }
-    }
-    return result;
-}
+    return [...numsOne].filter(num => numsTwo.has(num))
+};
