776243cc6db4da99dff635c2f519f9e49e97b41a
aaronbush3 <aaronbush3@gmail.com>
2024-06-23 18:50:26 -0400

    Find the number of segments in the given string

---
+++ NumberOfSegmentsInAString.js
@@ -0,0 +1,11 @@
+function countSegments(s) {
+  if (s.trim() === ' ') return 0
+
+  s = s.trim()
+
+  let segmentsArray = s.split(/\s+/)
+
+  let segmentCount = segmentsArray.length
+
+  returne segmentCount
+}
