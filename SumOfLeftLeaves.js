0eb25fb4d0241609e5974ec65a86e2e5c34f1761
aaronbush3 <aaronbush3@gmail.com>
2024-07-14 18:12:53 -0400

    Find the sum of the left leaves in a binary tree

---
+++ SumOfLeftLeaves.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if (!root) return 0
+
+    let sum = 0
+
+    const isLeaf = (node) => !node.left && !node.right
+    const dfs = (node, isLeft) => {
+        if (!node) return
+
+        if (isLeaf(node) && isLeft) {
+            sum += node.val
+        } 
+
+        dfs(node.left, true)
+        dfs(node.right, false)
+    }
+
+    dfs(root, false)
+
+    return sum
+};
